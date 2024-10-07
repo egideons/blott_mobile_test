4060cbc3d938fe92efaeaf409b247a0b707ac4a2
Gideon <gideon.dart@gmail.com>
2024-10-07 15:31:35 +0100

    - added fonts - changed native splash icon - changed app icon

--- lib/src/constants/assets.dart
+++ lib/src/constants/assets.dart
@@ -1,14 +1,14 @@
 class Assets {
 //=================== Images and Icons ===================\\
   //App Icon
-  static const appIconPng = "assets/image/png/app_icon.png";
-  static const appIconSvg = "assets/image/svg/app_icon.svg";
+  static const appIconPng = "assets/images/png/app_icon.png";
+  static const appIconSvg = "assets/images/svg/app_icon.svg";
   static const darkAppIcon = "assets/image/png/app_icon_dark_bg.png";
 
   //Icons
 
 //Placeholder Image
-  static const placeholderImage = "assets/image/png/placeholder_image.png";
+  static const placeholderImage = "assets/images/png/placeholder_image.png";
   static const userPlaceholderImage =
       "https://img.freepik.com/free-psd/3d-icon-social-media-app_23-2150049569.jpg";
 
